0b92027efd7a6430d1ee8a99a5a8ce1e79bda90a
YGAO0114 <ygao130@jh.edu>
2025-11-03 20:10:14 -0500

    save bmi

--- roster.py
+++ roster.py
@@ -35,3 +35,15 @@ player = {'Last Name': ['Bacot', 'Davis', 'Cadeau'],
           'weight': [240,180,180]}
 data = pd.DataFrame(player)
 print(data)
+
+# Step 7: 
+player = {'Last Name': ['Bacot', 'Davis', 'Cadeau'],
+          'First Name': ['Armando', 'RJ', 'Elliot'],
+          'height': [83,72,73],
+          'weight': [240,180,180]}
+data = pd.DataFrame(player)
+print(data)
+# bmi = weight in kg/ height in meters squared
+data ["bmi"] = (data ["weight"]/2.205)/((data ["height"]/39.37)**2)
+print (data)
+data. to_csv("bmi.csv" )
