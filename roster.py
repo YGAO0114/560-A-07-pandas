805941130b25442564b99f62fe14be18551af0e5
YGAO0114 <ygao130@jh.edu>
2025-11-03 20:09:11 -0500

    multiple headers and columns

--- roster.py
+++ roster.py
@@ -18,3 +18,12 @@ print(data)
 player = {'Last Name': roster}
 data = pd.DataFrame(player)
 print(data)
+
+
+# Step 5:  DataFrame with different columns
+player = {'Last Name': roster,
+          'First Name': ['Armando', 'RJ', 'Elliot'],
+          'height': [83,72,73],
+          'weight': [240,180,180]}
+data = pd.DataFrame(player)
+print(data)
