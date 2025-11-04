4d6937388fc24e98202718d2cd4c36bfcf9a3a56
YGAO0114 <ygao130@jh.edu>
2025-11-03 20:07:29 -0500

    roster loop

--- roster.py
+++ roster.py
@@ -3,17 +3,9 @@ import pandas as pd
 
 
 # Step 1: Simple list with last names only
-roster = ['Bacot', 'Davis', 'Cadeau', 'Ryan', 'Ingram', 'Withers', 'Trimble', 'Washington', 'Wojcik', 'High']
+roster = ['Bacot', 'Davis', 'Cadeau']
 # print(roster)
 
 # Step 2: Print using a for loop
 for player in roster:
     print(player)
-
-# Step 3: Import Pandas and create DataFrame with no header
-data = pd.DataFrame(roster)
-print(data)
-
-# Step 4: DataFrame with 'Last Name' header
-data = pd.DataFrame(roster, columns=['Last Name'])
-print(data)
