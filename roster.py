122046dc1a6fc5fb00187da976c475c3552956b5
YGAO0114 <ygao130@jh.edu>
2025-11-03 20:08:06 -0500

    0 header

--- roster.py
+++ roster.py
@@ -9,3 +9,7 @@ roster = ['Bacot', 'Davis', 'Cadeau']
 # Step 2: Print using a for loop
 for player in roster:
     print(player)
+
+# Step 3: Import Pandas and create DataFrame with no header
+data = pd.DataFrame(roster)
+print(data)
