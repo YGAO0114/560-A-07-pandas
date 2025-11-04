12707168991f912c3fc516b27af7f889db4d6574
YGAO0114 <ygao130@jh.edu>
2025-11-03 20:09:51 -0500

    type Last Name column content

--- roster.py
+++ roster.py
@@ -27,3 +27,11 @@ player = {'Last Name': roster,
           'weight': [240,180,180]}
 data = pd.DataFrame(player)
 print(data)
+
+# Step 6: 
+player = {'Last Name': ['Bacot', 'Davis', 'Cadeau'],
+          'First Name': ['Armando', 'RJ', 'Elliot'],
+          'height': [83,72,73],
+          'weight': [240,180,180]}
+data = pd.DataFrame(player)
+print(data)
